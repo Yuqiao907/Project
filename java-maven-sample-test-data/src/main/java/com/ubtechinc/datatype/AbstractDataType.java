2b945b845a8b376a1c90fadb2e6b79930a083a1d
Yuqiao Zhu <vivienyuqiaozhu@gmail.com>
2025-07-30 21:59:31 -0400

    格式化

--- java-maven-sample-test-data/src/main/java/com/ubtechinc/datatype/AbstractDataType.java
+++ java-maven-sample-test-data/src/main/java/com/ubtechinc/datatype/AbstractDataType.java
@@ -1,35 +1,40 @@
 package com.ubtechinc.datatype;
 
+/**
+ * Abstract datatype
+ *
+ * @author MacBook Air
+ * @date 2025/7/30 21:48
+ */
 
 public abstract class AbstractDataType<datatype> {
 
 
-        protected datatype defaultValue;
+    protected datatype defaultValue;
 
-        public AbstractDataType(datatype defauldatatypeValue) {
-            this.defaultValue = defaultValue;
-        }
+    public AbstractDataType(datatype defauldatatypeValue) {
+        this.defaultValue = defaultValue;
+    }
 
     /**
-     *
      * @return default value
      */
 
-        public datatype getDefault() {
-            return defaultValue;
-        }
+    public datatype getDefault() {
+
+        return defaultValue;
+    }
 
     /**
-     *
-     * @param value
+     * @param value -value wants to assign to
      */
-        public void setDefault(datatype value) {
-            this.defaultValue = value;
-        }
-
-
-        public abstract void formalForm();
+    public void setDefault(datatype value) {
+        this.defaultValue = value;
     }
+
+
+    public abstract void formalForm();
+}
 
 
 
